4ae7b034a8698aaad6977c35866432209bb10979
Alexander Håkansson <hsson@brkt.com>
2018-03-19 11:28:30 -0700

    Use new organization.json file
    Make sure the new organization.json file is used for all olga
    operations. This also removes all usage of the old accounts JSON
    file. Did some testing with both AWS and GCP to make sure
    everything still works.
    
    Also did some minor un-related changes that I found along the
    way while refactoring.
    
    Change-Id: I0b384812aeb437556f800c216aec69a36f25dc03
    Jira: YOLO-991

--- housekeeper/notify/helpers.go
+++ housekeeper/notify/helpers.go
@@ -4,7 +4,6 @@ import (
 	"brkt/cloudsweeper/cloud"
 	"brkt/cloudsweeper/cloud/billing"
 	"brkt/cloudsweeper/cloud/filter"
-	"brkt/cloudsweeper/housekeeper"
 	"brkt/cloudsweeper/mailer"
 	"bytes"
 	"fmt"
@@ -106,12 +105,11 @@ func extraTemplateFunctions() template.FuncMap {
 				return ""
 			}
 		},
-		"maybeNameToID": func(name string, owners housekeeper.Owners) string {
-			nameToIDMap := owners.NameToID()
-			if ID, ok := nameToIDMap[name]; ok {
-				return ID
+		"maybeRealName": func(account string, accountToUser map[string]string) string {
+			if name, ok := accountToUser[account]; ok {
+				return name
 			}
-			return ""
+			return account
 		},
 		"prettyTag": func(key, val string) string {
 			if val == "" {
